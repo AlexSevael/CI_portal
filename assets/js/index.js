8044971b6eeecbe4c114b5b95d7690015ba0ee33
Alexandar K <alex@sevael.com>
2022-10-25 10:52:32 +0530

    added ci portal

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+function logout() {
+    window.location = '/login.html';
+}
+function homepage(){
+   window.location = '/index.html';
+}
+function user_master_page(){
+   window.location = '/index.html';
+}
+function partconsumtoin_page(){
+   window.location = '/partconsumption.html';
+}
+function add_new(){
+   window.location = '/add_user_master.html';
+}
+function clickFun() {
+    window.location = '/index.html';
+}
+function back_add_user(){
+   user_master_page()
+}
