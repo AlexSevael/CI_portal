26e4191f4cb8c1ec0ee70b6316a4d37d8f2d5775
Alexandar K <alex@sevael.com>
2022-10-25 11:09:14 +0530

    latest changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,20 @@
 function logout() {
-    window.location = '/login.html';
+    window.location = 'login.html';
 }
 function homepage(){
-   window.location = '/index.html';
+   window.location = 'index.html';
 }
 function user_master_page(){
-   window.location = '/index.html';
+   window.location = 'index.html';
 }
 function partconsumtoin_page(){
-   window.location = '/partconsumption.html';
+   window.location = 'partconsumption.html';
 }
 function add_new(){
-   window.location = '/add_user_master.html';
+   window.location = 'add_user_master.html';
 }
 function clickFun() {
-    window.location = '/index.html';
+    window.location = 'index.html';
 }
 function back_add_user(){
    user_master_page()
