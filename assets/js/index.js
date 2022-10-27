42ac4c035e3d98d073cabf507340366f1bed0075
Alexandar K <alex@sevael.com>
2022-10-27 12:34:27 +0530

    change login format

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,10 +2,10 @@ function logout() {
     window.location = 'login.html';
 }
 function homepage(){
-   window.location = 'index.html';
+   window.location = 'user_master.html';
 }
 function user_master_page(){
-   window.location = 'index.html';
+   window.location = 'user_master.html';
 }
 function partconsumptoin_page(){
    window.location = 'partconsumption.html';
@@ -13,13 +13,12 @@ function partconsumptoin_page(){
 function add_new(){
    window.location = 'add_user_master.html';
 }
-function clickFun() {
-    window.location = 'index.html';
+function login() {
+    window.location = 'user_master.html';
 }
 function back_add_user(){
    user_master_page()
 }
-
 function save_close(){
    window.location = '#';
 }
